b4415c7bfc2a9f41774eccc297005350aa04443e
Dang Vu <tamquattnb@gmail.com>
2024-10-12 20:25:31 +0100

    update pre-commit git hook

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      This is the footer
     </footer>
   );
 };
