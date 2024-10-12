fe0d722f008ce3532de26e8107423a2aae843761
Dang Vu <tamquattnb@gmail.com>
2024-10-12 15:46:38 +0100

    chore: setup prettier and pre-commit hook with lint-staged

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(['/buttons(.*)'])
+const isProtectedRoute = createRouteMatcher(['/buttons(.*)']);
 
 export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect()
-  })
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: [
