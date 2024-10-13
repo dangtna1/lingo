410504b23b89f184db18be51a676b8388fe8d8b6
Dang Vu <tamquattnb@gmail.com>
2024-10-13 17:06:16 +0100

    chore: implement learn page wrapper

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,29 @@
-const Learn = () => {
-  return <>Learn</>;
+import { FeedWrapper } from '@/components/feed-wrapper';
+import { StickyWrapper } from '@/components/sticky-wrapper';
+import { Header } from './header';
+import { UserProgress } from '@/components/user-progress';
+
+const LearnPage = () => {
+  return (
+    <div className="flex flex-row-reverse gap-[40px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: 'Spanish', imageSrc: '/es.svg' }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title={'Spanish'} />
+        <div className="space-y-4">
+          <div className="h-[700px] bg-blue-500 w-full"></div>
+          <div className="h-[700px] bg-blue-500 w-full"></div>
+          <div className="h-[700px] bg-blue-500 w-full"></div>
+        </div>
+      </FeedWrapper>
+    </div>
+  );
 };
 
-export default Learn;
+export default LearnPage;
