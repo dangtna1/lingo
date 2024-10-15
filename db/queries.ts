739da810c9cff9251eca5e51e5f50f5c4666bf01
Dang Vu <tamquattnb@gmail.com>
2024-10-15 22:02:16 +0100

    chore: update shorter way to query data

--- db/queries.ts
+++ db/queries.ts
@@ -17,43 +17,13 @@ export const getUserProgress = cache(async () => {
     return null;
   }
 
-  // Fetch user progress for the specified user
-  const userProgressEntry = await db
-    .select()
-    .from(userProgress)
-    .where(eq(userProgress.userId, userId))
-    .limit(1); // Limit to 1 record
+  const userProgress = await db.query.userProgress.findFirst({
+    with: {
+      activeCourse: true,
+    },
+  });
 
-  // Check if user progress data exists
-  if (userProgressEntry.length === 0) {
-    return null;
-  }
-
-  // Get the first entry from the user progress data
-  const progress = userProgressEntry[0];
-
-  // Initialize activeCourse
-  let activeCourse = null;
-
-  // Fetch the active course if an activeCourseId exists
-  if (progress.activeCourseId) {
-    const courseData = await db
-      .select()
-      .from(courses)
-      .where(eq(courses.id, progress.activeCourseId))
-      .limit(1); // Limit to 1 record
-
-    // If a course is found, assign it to activeCourse
-    if (courseData.length > 0) {
-      activeCourse = courseData[0]; // Get the first course
-    }
-  }
-
-  // Return the user progress along with the active course
-  return {
-    ...progress,
-    activeCourse, // This will be the course details or null if not found
-  };
+  return userProgress;
 });
 
 export const getCourseById = cache(async (courseId: number) => {
